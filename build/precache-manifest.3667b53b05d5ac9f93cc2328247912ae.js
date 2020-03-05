f5315aa638a952756d39b194d7a4bdf728561951
Ida Lindell <ida.lindahl@gmail.com>
2020-03-05 13:39:43 +0100

    more package.json and gitignore changes

---
+++ build/precache-manifest.3667b53b05d5ac9f93cc2328247912ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5196de999bf5b0d65b156f2459494c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4a7cfcbc326fd8b5b8",
+    "url": "/static/css/main.fa6b7578.chunk.css"
+  },
+  {
+    "revision": "35b758b1d9f2f772ceb3",
+    "url": "/static/js/2.b9a5a563.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b9a5a563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4a7cfcbc326fd8b5b8",
+    "url": "/static/js/main.51633f7e.chunk.js"
+  },
+  {
+    "revision": "19e4183122765d51670a",
+    "url": "/static/js/runtime-main.8b93d233.js"
+  },
+  {
+    "revision": "505a442a9c0477a41ebff922bb63ce9b",
+    "url": "/static/media/cake.505a442a.png"
+  }
+]);
